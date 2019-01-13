f1260073e38c1fa6b321364799c0af6d9a8c7bed
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-13 15:12:37 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,13 +1,10 @@
 def my_select(array)
   i = 0 
-  new_collection = []
-  while i < collection.length 
-    if yield (collection[i])
-       if x == true 
-         new_collection << collection[i]
-       else 
-         collection.pop(i)
-       end
+  new_array = []
+  while i < array.length 
+    if yield (array[i]) == true
+      new_array << array[i]
+    end
     i += 1 
   end
   
