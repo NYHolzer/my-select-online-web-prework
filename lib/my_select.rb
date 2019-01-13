38da67e9014c6d3bf58f41f3079191ba3c886e5e
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-13 14:50:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,8 @@ def my_select(collection)
   i = 0 
   
   while i < collection.length 
-    yield if block_given? (collection[i])
+    yield(arg) if block_given? 
+       (collection[i])
     i += 1 
   end
 end
