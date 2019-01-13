ac58860138fe2e4ad08581a90e76cfabeade5a16
Nissan Holzer <nissan.holzer@gmail.com>
2019-01-13 14:52:20 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -4,7 +4,7 @@ def my_select(collection)
   while i < collection.length 
     yield(arg) 
        if (collection[i]).(arg) == true 
-         
+         new_collection << collection[i]
     i += 1 
   end
 end
